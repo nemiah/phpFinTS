4655759482c54485a2f121177255ca733257e98b
mschindler83 <markus.schindler1@googlemail.com>
2016-07-08 12:57:21 +0200

    Update statement_of_account.php

--- Samples/statement_of_account.php
+++ Samples/statement_of_account.php
@@ -10,7 +10,7 @@ require '../vendor/autoload.php';
 use Fhp\FinTs;
 use Fhp\Model\StatementOfAccount\Statement;
 
-define('FHP_BANK_URL', '');                # HBCI / FinTS Url can be found here: https://www.hbci-zka.de/institute/institut_auswahl.htm
+define('FHP_BANK_URL', '');                # HBCI / FinTS Url can be found here: https://www.hbci-zka.de/institute/institut_auswahl.htm (use the PIN/TAN URL)
 define('FHP_BANK_PORT', 443);              # HBCI / FinTS Port can be found here: https://www.hbci-zka.de/institute/institut_auswahl.htm
 define('FHP_BANK_CODE', '');               # Your bank code / Bankleitzahl
 define('FHP_ONLINE_BANKING_USERNAME', ''); # Your online banking username / alias
