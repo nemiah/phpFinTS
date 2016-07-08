e7e007c8a668e465508e749ef82665d45125021c
Alexander Serbe <codiersklave@yahoo.de>
2016-07-08 12:36:06 +0200

    Updated comment for FHP_BANK_URL
    There are two URLs: the HBCI-URL and the PIN/TAN-URL; the right one to use is the PIN/TAN-URL.

--- Samples/saldo.php
+++ Samples/saldo.php
@@ -10,7 +10,7 @@ use Fhp\FinTs;
 use Fhp\Model\StatementOfAccount\Statement;
 use Fhp\Model\StatementOfAccount\Transaction;
 
-define('FHP_BANK_URL', '');                 # HBCI / FinTS Url can be found here: https://www.hbci-zka.de/institute/institut_auswahl.htm
+define('FHP_BANK_URL', '');                 # HBCI / FinTS Url can be found here: https://www.hbci-zka.de/institute/institut_auswahl.htm (use the PIN/TAN URL)
 define('FHP_BANK_PORT', 443);               # HBCI / FinTS Port can be found here: https://www.hbci-zka.de/institute/institut_auswahl.htm
 define('FHP_BANK_CODE', '');                # Your bank code / Bankleitzahl
 define('FHP_ONLINE_BANKING_USERNAME', '');  # Your online banking username / alias
