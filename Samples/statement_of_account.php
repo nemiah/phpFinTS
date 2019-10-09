a3f49b307f413bb17703ea2c384b7ae0de56c76d
Nena Furtmeier <Nena@Furtmeier.IT>
2019-10-09 10:16:49 +0200

    move logger from __construct to setLogger

--- Samples/statement_of_account.php
+++ Samples/statement_of_account.php
@@ -33,10 +33,10 @@ $fints = new FinTs(
     FHP_BANK_CODE,
     FHP_ONLINE_BANKING_USERNAME,
     FHP_ONLINE_BANKING_PIN,
-    new testLogger(),
     FHP_REGISTRATION_NO,
     FHP_SOFTWARE_VERSION
 );
+$fints->setLogger(new testLogger());
 
 try {
 	
@@ -73,11 +73,11 @@ try {
 			FHP_BANK_CODE,
 			FHP_ONLINE_BANKING_USERNAME,
 			FHP_ONLINE_BANKING_PIN,
-			null,
 			FHP_REGISTRATION_NO,
 			FHP_SOFTWARE_VERSION
 		);
-
+		$fints->setLogger(new testLogger());
+		
 		$soa = $fints->finishStatementOfAccount($unserialized, $oneAccount, $from, $to, $tan);
 	}
     $fints->end();
