cb4a56b045f3309f8c800733520c0c4287110841
Nena Furtmeier <Nena@Furtmeier.IT>
2019-10-09 10:16:16 +0200

    move logger from __construct to setLogger

--- Samples/standingOrders.php
+++ Samples/standingOrders.php
@@ -29,11 +29,10 @@ $fints = new FinTs(
     FHP_BANK_CODE,
     FHP_ONLINE_BANKING_USERNAME,
     FHP_ONLINE_BANKING_PIN,
-	new testLogger(),
     FHP_REGISTRATION_NO,
     FHP_SOFTWARE_VERSION
 );
-
+$fints->setLogger(new testLogger());
 $accounts = $fints->getSEPAAccounts();
 
 $orders = $fints->getSEPAStandingOrders($accounts[0]);
