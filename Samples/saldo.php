eea1b62f3642963088aa9de373c30f596e8c98fa
Nena Furtmeier <Nena@Furtmeier.IT>
2019-10-09 10:15:17 +0200

    move logger from __construct to setLogger

--- Samples/saldo.php
+++ Samples/saldo.php
@@ -32,10 +32,10 @@ $fints = new FinTs(
     FHP_BANK_CODE,
     FHP_ONLINE_BANKING_USERNAME,
     FHP_ONLINE_BANKING_PIN,
-	new testLogger(),
     FHP_REGISTRATION_NO,
     FHP_SOFTWARE_VERSION
 );
+$fints->setLogger(new testLogger());
 
 $accounts = $fints->getSEPAAccounts();
 
