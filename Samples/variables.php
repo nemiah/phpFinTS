ec54380b6a4513bfa791eb22a43b8a20abff4edb
Nena Furtmeier <Nena@Furtmeier.IT>
2019-10-09 10:17:44 +0200

    move logger from __construct to setLogger

--- Samples/variables.php
+++ Samples/variables.php
@@ -28,10 +28,9 @@ $fints = new FinTs(
     FHP_BANK_CODE,
     FHP_ONLINE_BANKING_USERNAME,
     FHP_ONLINE_BANKING_PIN,
-    new testLogger(),
     FHP_REGISTRATION_NO,
     FHP_SOFTWARE_VERSION
 );
-
+$fints->setLogger(new testLogger());
 print_r($fints->getVariables());
 $fints->end();
