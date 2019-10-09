80568bbae780bbae096987e01f9151319d0c3f92
Nena Furtmeier <Nena@Furtmeier.IT>
2019-10-09 10:15:22 +0200

    move logger from __construct to setLogger

--- Samples/directDebit.php
+++ Samples/directDebit.php
@@ -66,11 +66,10 @@ $fints = new FinTs(
     FHP_BANK_CODE,
     FHP_ONLINE_BANKING_USERNAME,
     FHP_ONLINE_BANKING_PIN,
-	new testLogger(),
     FHP_REGISTRATION_NO,
     FHP_SOFTWARE_VERSION
 );
-
+$fints->setLogger(new testLogger());
 $fints->setTANMechanism(901); //901 for mobileTAN
 file_put_contents(__DIR__."/tan.txt", "");
 
