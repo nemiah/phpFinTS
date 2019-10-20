3b06c2651f3fabf0d7e1c69e323280f7b1d418e7
Philipp Keck <git@philippkeck.de>
2019-10-20 18:16:27 +0200

    Add missing documentation to Btg

--- lib/Fhp/Segment/Common/Btg.php
+++ lib/Fhp/Segment/Common/Btg.php
@@ -4,6 +4,12 @@ namespace Fhp\Segment\Common;
 
 use Fhp\Segment\BaseDeg;
 
+/**
+ * Class Btg
+ * Mehrfach verwendetes Element: Betrag (Version 1)
+ *
+ * @package Fhp\Segment\Common
+ */
 class Btg extends BaseDeg
 {
     /** @var float */
