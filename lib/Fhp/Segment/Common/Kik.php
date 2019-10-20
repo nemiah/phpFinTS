b7f160442249474a27c62e0436297aa0d82d30a7
Philipp Keck <git@philippkeck.de>
2019-10-20 18:25:21 +0200

    Implement common DEGs Kik, Kti and Ktz based on latest specification

---
+++ lib/Fhp/Segment/Common/Kik.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Fhp\Segment\Common;
+
+use Fhp\Segment\BaseDeg;
+
+/**
+ * Class Kik
+ * Mehrfach verwendetes Element: Kreditinstitutskennung (Version 1)
+ *
+ * @link https://www.hbci-zka.de/dokumente/spezifikation_deutsch/fintsv3/FinTS_3.0_Messages_Geschaeftsvorfaelle_2015-08-07_final_version.pdf
+ * Section: B.2
+ *
+ * @package Fhp\Segment\Common
+ */
+class Kik extends BaseDeg
+{
+    const DEFAULT_COUNTRY_CODE = '280'; // Germany
+
+    /** @var string (ISO 3166-1; has leading zeros; Germany is 280, see also chapter E.4 */
+    public $laenderkennzeichen;
+    /** @var string|null Max length: 30 (Mandatory/absent depending on the country) */
+    public $kreditinstitutscode;
+
+    /**
+     * @param string $kreditinstitutscode
+     * @return Kik
+     */
+    public static function create($kreditinstitutscode)
+    {
+        $result = new Kik();
+        $result->laenderkennzeichen = static::DEFAULT_COUNTRY_CODE;
+        $result->kreditinstitutscode = $kreditinstitutscode;
+        return $result;
+    }
+}
---
+++ lib/Fhp/Segment/Common/Ktz.php
@@ -0,0 +1,30 @@
+<?php /** @noinspection PhpUnused */
+
+namespace Fhp\Segment\Common;
+
+use Fhp\Segment\BaseDeg;
+
+/**
+ * Class Ktz
+ * Data Element Group: Kontoverbindung ZV international (Version 1)
+ *
+ * @link https://www.hbci-zka.de/dokumente/spezifikation_deutsch/fintsv3/FinTS_3.0_Messages_Geschaeftsvorfaelle_2015-08-07_final_version.pdf
+ * Section: B.3.3
+ *
+ * @package Fhp\Segment\Common
+ */
+class Ktz extends BaseDeg
+{
+    /** @var boolean Whether it's a SEPA account that has IBAN/BIC, or not (e.g. a stock depot) */
+    public $kontoverwendungSepa;
+    /** @var string|null Max length: 34 */
+    public $iban;
+    /** @var string|null Max length: 11, required if IBAN is present. */
+    public $bic;
+    /** @var string Also known as Depotnummer. */
+    public $kontonummer;
+    /** @var string|null */
+    public $unterkontomerkmal;
+    /** @var Kik */
+    public $kreditinstitutskennung;
+}
