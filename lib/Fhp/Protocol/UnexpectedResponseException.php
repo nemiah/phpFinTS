e8cc10b352aecd0ecac982e170c4a5cedb615ad8
Philipp Keck <git@philippkeck.de>
2019-10-30 21:57:26 +0100

    Add UnexpectedResponseException

---
+++ lib/Fhp/Protocol/UnexpectedResponseException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Fhp\Protocol;
+
+/**
+ * Thrown if the server responds with a syntactically valid message that does not match the protocol expectations
+ * implemented in this library.
+ */
+class UnexpectedResponseException extends \RuntimeException
+{
+    /**
+     * @param string $message
+     * @param int $code
+     * @param \Exception|null $previous
+     */
+    public function __construct($message, $code = 0, \Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
