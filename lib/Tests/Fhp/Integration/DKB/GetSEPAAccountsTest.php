c8600a3a533f25af4e63cf932ca6d9ce9722f5a6
Philipp Keck <git@philippkeck.de>
2019-11-24 00:58:37 +0100

    Add action GetSEPAAccounts plus integration tests

---
+++ lib/Tests/Fhp/Integration/DKB/GetSEPAAccountsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Fhp\Integration\DKB;
+
+class GetSEPAAccountsTest extends DKBIntegrationTestBase
+{
+    const GET_ACCOUNTS_REQUEST = "HKSPA:3:2'";
+    const GET_ACCOUNTS_RESPONSE = "HIRMG:3:2+0010::Nachricht entgegengenommen.'HIRMS:4:2:3+0020::Der Auftrag wurde ausgefuhrt.'HISPA:5:2:3+J:DExxABCDEFGH1234567890:BYLADEM1001:1234567890::280:ABCDEFGH'";
+
+    /**
+     * @throws \Throwable
+     */
+    public function test_getSEPAAccounts()
+    {
+        $this->initDialog();
+
+        $this->expectMessage(static::GET_ACCOUNTS_REQUEST, static::GET_ACCOUNTS_RESPONSE);
+        $getAccounts = new \Fhp\Action\GetSEPAAccounts();
+        $this->fints->execute($getAccounts);
+        $accounts = $getAccounts->getAccounts();
+
+        $this->assertCount(1, $accounts);
+        $account = $accounts[0];
+        $this->assertEquals('DExxABCDEFGH1234567890', $account->getIban());
+        $this->assertEquals('BYLADEM1001', $account->getBic());
+        $this->assertEquals('1234567890', $account->getAccountNumber());
+        $this->assertEmpty($account->getSubAccount());
+        $this->assertEquals('ABCDEFGH', $account->getBlz());
+    }
+}
