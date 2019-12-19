c39a3051180457fe4c5c11e9ca67d2a70a7353a4
ampaze <ampaze@gmail.com>
2019-12-19 11:48:33 +0100

    Verbesserungen für SendSEPADirectDebit Action

--- lib/Fhp/Action/SendSEPADirectDebit.php
+++ lib/Fhp/Action/SendSEPADirectDebit.php
@@ -12,11 +12,16 @@ use Fhp\Segment\DME\HIDMESv1;
 use Fhp\Segment\DME\HIDMESv2;
 use Fhp\Segment\DME\HIDXES;
 use Fhp\Segment\DME\HKDMEv1;
+use Fhp\Segment\DME\HKDMEv2;
 use Fhp\Segment\DSE\HIDSESv2;
 use Fhp\Segment\DSE\HKDSEv1;
+use Fhp\Segment\DSE\HKDSEv2;
 use Fhp\Segment\SPA\HISPAS;
 use Fhp\UnsupportedException;
 
+/**
+ * Initiate one or multiple SEPA Direct Debits ("Lastschriften")
+ */
 class SendSEPADirectDebit extends BaseAction
 {
     /** @var SEPAAccount */
@@ -26,7 +31,7 @@ class SendSEPADirectDebit extends BaseAction
     private $painMessage;
 
     /** @var string */
-    private $xmlSchema;
+    private $painNamespace;
 
     /** @var float */
     private $ctrlSum;
@@ -36,65 +41,91 @@ class SendSEPADirectDebit extends BaseAction
 
     public static function create(SEPAAccount $account, string $painMessage): SendSEPADirectDebit
     {
-        if (preg_match('/xmlns="(?<namespace>[^"]+)".*<GrpHdr>.*<CtrlSum>(?<ctrlsum>[.0-9]+)<\/CtrlSum>.*<\/GrpHdr>/s', $painMessage, $matches) === 1) {
-            $painUrn = $matches['namespace'];
-            $ctrlSum = $matches['ctrlsum'];
+        if (preg_match('/xmlns="(?<namespace>[^"]+)"/s', $painMessage, $matches) === 1) {
+            $painNamespace = $matches['namespace'];
         } else {
-            throw new \InvalidArgumentException('PAIN-Namespace and or <GrpHdr><CtrlSum>xx</CtrlSum></GrpHdr> missing in PAIN message');
+            throw new \InvalidArgumentException('The namespace aka "xmlns" is missing in PAIN message');
         }
 
-        // Check wether it is one DirectDebit or multiple, should match <NbOfTxs>xx</NbOfTxs> in the XML
+        // Check whether the PAIN message contains multiple or only one Direct Debit, should match <NbOfTxs>xx</NbOfTxs> in the XML
         $nbOfTxs = substr_count($painMessage, '<DrctDbtTxInf>');
+        $ctrlSum = null;
+
+        if (preg_match('/<GrpHdr>.*<CtrlSum>(?<ctrlsum>[.0-9]+)<\/CtrlSum>.*<\/GrpHdr>/s', $painMessage, $matches) === 1) {
+            $ctrlSum = $matches['ctrlsum'];
+        }
+
+        if ($nbOfTxs > 1 && is_null($ctrlSum)) {
+            throw new \InvalidArgumentException('The control sum aka "<GrpHdr><CtrlSum>xx</CtrlSum></GrpHdr>" is missing in PAIN message');
+        }
 
         $result = new SendSEPADirectDebit();
         $result->account = $account;
         $result->painMessage = $painMessage;
-        $result->xmlSchema = $painUrn;
+        $result->painNamespace = $painNamespace;
         $result->ctrlSum = $ctrlSum;
-        $result->singleDirectDebit = $nbOfTxs == 1;
+
+        $result->singleDirectDebit = $nbOfTxs === 1;
+
         return $result;
     }
 
     public function createRequest($bpd, $upd)
     {
-        $type = $this->singleDirectDebit ? 'S' : 'M';
+        $useSingleDirectDebit = $this->singleDirectDebit;
+
+        // If the PAIN message contains a control sum, we should use it, if the bank also supports it
+        if ($useSingleDirectDebit && !is_null($this->ctrlSum) && !is_null($bpd->getLatestSupportedParameters('HIDMES'))) {
+            $useSingleDirectDebit = false;
+        }
 
         /** @var HIDXES|BaseSegment $hidxes */
-        $hidxes = $bpd->requireLatestSupportedParameters('HID' . $type . 'ES');
+        $hidxes = $bpd->requireLatestSupportedParameters($useSingleDirectDebit ? 'HIDSES' : 'HIDMES');
 
-        $supportedSchemas = null;
+        $supportedPainNamespaces = null;
 
-        if ($hidxes->getVersion() == 2) {
+        if ($hidxes->getVersion() === 2) {
             /** @var HIDMESv2|HIDSESv2 $hidxes */
-            $supportedSchemas = $hidxes->getParameter()->unterstuetzteSEPADatenformate;
+            $supportedPainNamespaces = $hidxes->getParameter()->unterstuetzteSEPADatenformate;
         }
 
         // If there are no SEPA formats available in the HIDXES Parameters, we look to the general formats
-        if (!is_array($supportedSchemas) || count($supportedSchemas) == 0) {
+        if (!is_array($supportedPainNamespaces) || count($supportedPainNamespaces) === 0) {
             /** @var HISPAS $hispas */
             $hispas = $bpd->requireLatestSupportedParameters('HISPAS');
-            $supportedSchemas = $hispas->getParameter()->getUnterstuetzteSepaDatenformate();
+            $supportedPainNamespaces = $hispas->getParameter()->getUnterstuetzteSepaDatenformate();
         }
 
-        if (!in_array($this->xmlSchema, $supportedSchemas)) {
-            throw new UnsupportedException("The bank does not support the XML schema $this->xmlSchema, but only "
-                . implode(', ', $supportedSchemas));
+        if (!in_array($this->painNamespace, $supportedPainNamespaces)) {
+            throw new UnsupportedException("The bank does not support the XML schema $this->painNamespace, but only "
+                . implode(', ', $supportedPainNamespaces));
         }
 
         /** @var HKDMEv1|HKDSEv1 $hkdxe */
-        $hkdxe = ('Fhp\Segment\D' . $type . 'E\HKD' . $type . 'Ev' . $hidxes->getVersion())::createEmpty();
+        $hkdxe = null;
+        switch ($hidxes->getVersion()) {
+            case 1:
+                $hkdxe = $useSingleDirectDebit ? HKDSEv1::createEmpty() : HKDMEv1::createEmpty();
+            break;
+            case 2:
+                $hkdxe = $useSingleDirectDebit ? HKDSEv2::createEmpty() : HKDMEv2::createEmpty();
+            break;
+            default:
+                throw new UnsupportedException('Unsupported HKDME or HKDSE version: ' . $hidxes->getVersion());
+        }
 
         $hkdxe->kontoverbindungInternational = Kti::fromAccount($this->account);
-        $hkdxe->sepaDescriptor = $this->xmlSchema;
+        $hkdxe->sepaDescriptor = $this->painNamespace;
         $hkdxe->sepaPainMessage = new Bin($this->painMessage);
 
-        if (!$this->singleDirectDebit) {
+        if ($useSingleDirectDebit) {
             $hkdxe->einzelbuchungGewuenscht = false;
 
-            /** @var HIDMESv1 $hidxes */
-            if ($hidxes->getParameter()->summenfeldBenoetigt) {
-                $hkdxe->summenfeld = new Btg($this->ctrlSum);
-            }
+            /* @var HIDMESv1 $hidxes */
+            // Just always send the control sum
+            //if ($hidxes->getParameter()->summenfeldBenoetigt) {
+            $hkdxe->summenfeld = Btg::create($this->ctrlSum);
+            //}
         }
 
         return $hkdxe;
