b8dc2533ed2587b2287ba85c76a5aaeb62bf2079
Philipp Keck <git@philippkeck.de>
2019-12-25 16:33:55 +0100

    Fix PHP7.3 compatibility of return type hints

--- lib/Fhp/Segment/DSE/HIDSESv1.php
+++ lib/Fhp/Segment/DSE/HIDSESv1.php
@@ -4,6 +4,7 @@ namespace Fhp\Segment\DSE;
 
 use Fhp\Segment\BaseGeschaeftsvorfallparameter;
 use Fhp\Segment\DME\HIDXES;
+use Fhp\Segment\DME\SEPADirectDebitMinimalLeadTimeProvider;
 
 /**
  * Segment: Terminierte SEPA-Einzellastschrift einreichen Parameter
@@ -16,7 +17,7 @@ class HIDSESv1 extends BaseGeschaeftsvorfallparameter implements HIDXES
     /** @var ParameterTerminierteSEPAEinzellastschriftEinreichenV1 */
     public $parameter;
 
-    public function getParameter(): ParameterTerminierteSEPAEinzellastschriftEinreichenV1
+    public function getParameter(): SEPADirectDebitMinimalLeadTimeProvider
     {
         return $this->parameter;
     }
