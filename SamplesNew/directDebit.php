a9ce7e4896486f85f1688d4da8d8c439fc4f5bc5
fbett <info@bett-ingenieure.de>
2020-02-10 17:26:15 +0100

    SamplesNew: Direct Debit

---
+++ SamplesNew/directDebit.php
@@ -0,0 +1,44 @@
+<?php
+
+/** @noinspection PhpUnhandledExceptionInspection */
+
+/**
+ * SAMPLE - Send a direct debit request
+ */
+
+// See login.php, it returns a FinTs instance that is already logged in.
+/** @var \Fhp\FinTsNew $fints */
+$fints = require_once 'login.php';
+
+// Just pick the first account, for demonstration purposes. You could also have the user choose, or have SEPAAccount
+// hard-coded and not call getSEPAAccounts() at all.
+$getSepaAccounts = \Fhp\Action\GetSEPAAccounts::create();
+$fints->execute($getSepaAccounts);
+if ($getSepaAccounts->needsTan()) {
+    handleTan($getSepaAccounts); // See login.php for the implementation.
+}
+$oneAccount = $getSepaAccounts->getAccounts()[0];
+
+// generate a SepaDirectDebit object (pain.008.002.02).
+$directDebitFile = new \AbcAeffchenSephpa\SephpaDirectDebit(
+    'Name of Application',
+    'Message Identifier',
+    \AbcAeffchenSephpa\SephpaDirectDebit::SEPA_PAIN_008_003_02
+);
+/*
+ *
+ * Configure the Direct Debit File
+ * $directDebitCollection = $directDebitFile->addCollection([...]);
+ * $directDebitCollection->addPayment([...]);
+ *
+ * See documentation:
+ * https://github.com/AbcAeffchen/Sephpa
+ *
+*/
+$xml = $directDebitFile->generateXml(date("Y-m-d\TH:i:s", time()));
+
+$sendSEPADirectDebit = \Fhp\Action\SendSEPADirectDebit::create($oneAccount, $xml);
+$fints->execute($sendSEPADirectDebit);
+if ($sendSEPADirectDebit->needsTan()) {
+    handleTan($getStatement); // See login.php for the implementation.
+}
