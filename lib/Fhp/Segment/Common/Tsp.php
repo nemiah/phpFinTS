917e7e238045718160748271272688099ec7b73b
Philipp Keck <git@philippkeck.de>
2020-04-09 16:33:02 +0200

    Add Tsp DEG for timestamps, plus unit test

---
+++ lib/Fhp/Segment/Common/Tsp.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Fhp\Segment\Common;
+
+use Fhp\Segment\BaseDeg;
+
+/**
+ * Mehrfach verwendetes Element: Zeitstempel (Version 1)
+ *
+ * @link https://www.hbci-zka.de/dokumente/spezifikation_deutsch/fintsv3/FinTS_3.0_Messages_Geschaeftsvorfaelle_2015-08-07_final_version.pdf
+ * Section: B.6
+ */
+class Tsp extends BaseDeg
+{
+    /** @var string JJJJMMTT gemäß ISO 8601 */
+    public $datum;
+    /** @var string|null hhmmss gemäß ISO 8601, local time (no time zone support). */
+    public $uhrzeit;
+
+    public static function create(string $datum, ?string $uhrzeit): Tsp
+    {
+        $result = new Tsp();
+        $result->datum = $datum;
+        $result->uhrzeit = $uhrzeit;
+        return $result;
+    }
+
+    public function asDateTime(): \DateTime
+    {
+        return \DateTime::createFromFormat('Ymd His', $this->datum . ' ' . ($this->uhrzeit ?? '000000'));
+    }
+}
---
+++ lib/Tests/Fhp/Segment/Common/TspTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Fhp\Segment\Common;
+
+use Fhp\Segment\Common\Tsp;
+
+class TspTest extends \PHPUnit\Framework\TestCase
+{
+    public function test_parse_withoutTime()
+    {
+        $this->assertEquals(new \DateTime('2020-01-02T00:00:00'), Tsp::parse('20200102')->asDateTime());
+        $this->assertEquals(new \DateTime('2020-07-02T00:00:00'), Tsp::parse('20200702')->asDateTime());
+    }
+
+    public function test_parse_withTime()
+    {
+        $this->assertEquals(new \DateTime('2020-01-02T11:22:33'), Tsp::parse('20200102:112233')->asDateTime());
+        $this->assertEquals(new \DateTime('2020-01-02T22:00:00'), Tsp::parse('20200102:220000')->asDateTime());
+    }
+}
