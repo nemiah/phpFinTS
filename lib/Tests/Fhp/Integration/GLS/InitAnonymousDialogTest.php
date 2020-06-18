0031795af8426cc154ba0a2b0eaaabfe062c38bc
ampaze <ampaze@gmail.com>
2020-06-18 14:38:59 +0200

    Test for GLS anonymous dialog

---
+++ lib/Tests/Fhp/Integration/GLS/InitAnonymousDialogTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Fhp\Integration\GLS;
+
+class InitAnonymousDialogTest extends GLSIntegrationTestBase
+{
+    /**
+     * @throws \Fhp\Protocol\ServerException
+     * @throws \Throwable
+     */
+    public function test_init_anonymous()
+    {
+        $this->InitAnonymous();
+    }
+}
