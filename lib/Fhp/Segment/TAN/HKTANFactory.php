b2fbe770540196bb859f9925111bb629fd3219b5
Philipp Keck <git@philippkeck.de>
2020-12-05 21:20:47 +0100

    Add a little more documentation to HKTANFactory

--- lib/Fhp/Segment/TAN/HKTANFactory.php
+++ lib/Fhp/Segment/TAN/HKTANFactory.php
@@ -7,10 +7,17 @@ use Fhp\Segment\BaseSegment;
 
 /**
  * Creates HKTAN segments matching the segment version used by the server.
+ *
+ * @link https://www.hbci-zka.de/dokumente/spezifikation_deutsch/fintsv3/FinTS_3.0_Security_Sicherheitsverfahren_PINTAN_2020-07-10_final_version.pdf
+ * Section D (look for "TAN-Prozess" version 1)
  */
 class HKTANFactory
 {
     /**
+     * This is TAN-Prozess=4, which is the first step of Prozessvariante 2. In this step, the client application sends
+     * a main action segment together with a HKTAN segment, in order to indicate that it is prepared to authenticate the
+     * action with a TAN if the server asks for it (which would trigger step 2 below).
+     *
      * @param TanMode $tanMode Parameters retrieved from the server during dialog initialization that describe how the
      *     TAN processes need to be parameterized.
      * @param ?string $tanMedium The TAN medium selected by the user. Mandatory if $tanMode is present and requires a
@@ -37,6 +44,10 @@ class HKTANFactory
     }
 
     /**
+     * This is TAN-Prozess=2, which is the second step of Prozessvariante 2. If the bank server asked for a TAN in step
+     * 1 above, then the client application sends that TAN in a HKTAN segment to the server in order to authenticate the
+     * previously transmitted action.
+     *
      * @param TanMode $tanMode Parameters retrieved from the server during dialog initialization that describe how the
      *     TAN processes need to be parameterized.
      * @param string $auftragsreferenz The reference number received from the server in step 1 response (HITAN).
