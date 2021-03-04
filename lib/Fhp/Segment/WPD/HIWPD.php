78455614512ded18f2175689907b6af2f07b94fc
ZwenAusZwota <sven.schuberth@gmx.de>
2021-03-04 17:39:41 +0100

    Create HIWPD.php

---
+++ lib/Fhp/Segment/WPD/HIWPD.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Fhp\Segment\WPD;
+
+use Fhp\Segment\Common\AccountInfo;
+use Fhp\Segment\Common\Btg;
+use Fhp\Segment\Common\Sdo;
+use Fhp\Segment\Common\Tsp;
+use Fhp\Segment\SegmentInterface;
+
+/**
+ * Segment: Depotaufstellung Rückmeldung
+ */
+interface HIWPD extends SegmentInterface
+{
+    
+}
