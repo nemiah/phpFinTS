ba8b109f156ade1abc423a0164399203cf00b4da
ZwenAusZwota <sven.schuberth@gmx.de>
2021-03-04 17:41:58 +0100

    Create HKWPDv5.php

---
+++ lib/Fhp/Segment/WPD/HKWPDv5.php
@@ -0,0 +1,34 @@
+<?php /** @noinspection PhpUnused */
+
+namespace Fhp\Segment\WPD;
+
+use Fhp\Segment\BaseSegment;
+use Fhp\Segment\Paginateable;
+
+/**
+ * Segment: Kontoumsätze anfordern/Zeitraum (Version 6)
+ *
+ * @link https://www.hbci-zka.de/dokumente/spezifikation_deutsch/fintsv3/FinTS_3.0_Messages_Geschaeftsvorfaelle_2015-08-07_final_version.pdf
+ * Section: C.4.3.1
+ */
+class HKWPDv5 extends BaseSegment implements Paginateable
+{
+    /** @var \Fhp\Segment\Common\KtvV3 */
+    public $kontoverbindungAuftraggeber;
+    
+    /** @var int|null Only allowed if {@link ParameterKontoumsaetzeV2::$eingabeAnzahlEintraegeErlaubt} says so. */
+    public $maximaleAnzahlEintraege;
+
+
+    public static function create(\Fhp\Segment\Common\KtvV3 $ktv): HKWPDv5
+    {
+        $result = HKWPDv5::createEmpty();
+        $result->kontoverbindungAuftraggeber = $ktv;
+        return $result;
+    }
+
+    public function setPaginationToken(string $paginationToken)
+    {
+        $this->aufsetzpunkt = $paginationToken;
+    }
+}
