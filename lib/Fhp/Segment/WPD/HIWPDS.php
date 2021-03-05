006ccbaf9810b1717f29e0163bb02c7a95133cb8
ZwenAusZwota <sven.schuberth@gmx.de>
2021-03-05 09:58:55 +0100

    Update HIWPDS.php
    Segment description updated

--- lib/Fhp/Segment/WPD/HIWPDS.php
+++ lib/Fhp/Segment/WPD/HIWPDS.php
@@ -5,7 +5,10 @@ namespace Fhp\Segment\WPD;
 use Fhp\Segment\SegmentInterface;
 
 /**
- * Segment: Kontoumsätze/Zeitraum Parameter
+ * Segment: Depotaufstellung Parameter
+ *
+ * @link https://www.hbci-zka.de/dokumente/spezifikation_deutsch/fintsv3/FinTS_3.0_Messages_Geschaeftsvorfaelle_2015-08-07_final_version.pdf
+ * Section: C.4.3.1c
  */
 interface HIWPDS extends SegmentInterface
 {
