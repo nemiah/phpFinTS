fdd90196b3cd23e127d2cb69a300cb2a01351ba0
ZwenAusZwota <sven.schuberth@gmx.de>
2021-03-05 10:17:56 +0100

    Create Kursqualitaet.php

---
+++ lib/Fhp/Segment/Common/Kursqualitaet.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Fhp\Segment\Common;
+
+use Fhp\Segment\BaseDeg;
+
+/**
+ * Mehrfach verwendetes Element: Kursqualität (Version 2)
+ *
+ * @link https://www.hbci-zka.de/dokumente/spezifikation_deutsch/fintsv3/FinTS_3.0_Messages_Geschaeftsvorfaelle_2015-08-07_final_version.pdf
+ * Section: D
+ */
+class Kursqualitaet extends BaseDeg
+{
+    const DELAYED = 1;  // delayed-Kurs
+    const REALTIME = 2; // Echtzeit-Kurs
