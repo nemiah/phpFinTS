0f4e86baf6610ed09156b3bccff3102e3e622d52
ZwenAusZwota <sven.schuberth@gmx.de>
2021-03-05 09:56:10 +0100

    Update HKWPDv5.php
    updated Segment description

--- lib/Fhp/Segment/WPD/HKWPDv5.php
+++ lib/Fhp/Segment/WPD/HKWPDv5.php
@@ -6,7 +6,7 @@ use Fhp\Segment\BaseSegment;
 use Fhp\Segment\Paginateable;
 
 /**
- * Segment: Kontoumsätze anfordern/Zeitraum (Version 6)
+ * Segment: Depotaufstellung anfordern (Version 5)
  *
  * @link https://www.hbci-zka.de/dokumente/spezifikation_deutsch/fintsv3/FinTS_3.0_Messages_Geschaeftsvorfaelle_2015-08-07_final_version.pdf
  * Section: C.4.3.1
