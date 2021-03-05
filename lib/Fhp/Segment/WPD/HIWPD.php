78ec6dfcc1ad769908033dec4a12e086dcb8acdc
ZwenAusZwota <sven.schuberth@gmx.de>
2021-03-05 09:57:34 +0100

    Update HIWPD.php
    Segment description updated

--- lib/Fhp/Segment/WPD/HIWPD.php
+++ lib/Fhp/Segment/WPD/HIWPD.php
@@ -10,6 +10,9 @@ use Fhp\Segment\SegmentInterface;
 
 /**
  * Segment: Depotaufstellung Rückmeldung
+ *
+ * @link https://www.hbci-zka.de/dokumente/spezifikation_deutsch/fintsv3/FinTS_3.0_Messages_Geschaeftsvorfaelle_2015-08-07_final_version.pdf
+ * Section: C.4.3.1b
  */
 interface HIWPD extends SegmentInterface
 {
