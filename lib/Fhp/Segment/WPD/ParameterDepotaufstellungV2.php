ec608ce388ff7b8f647d5ddfd332f9cdb4d908cf
ZwenAusZwota <sven.schuberth@gmx.de>
2021-03-05 10:05:52 +0100

    DEG Description updated

--- lib/Fhp/Segment/WPD/ParameterDepotaufstellungV2.php
+++ lib/Fhp/Segment/WPD/ParameterDepotaufstellungV2.php
@@ -5,6 +5,9 @@ namespace Fhp\Segment\WPD;
 use Fhp\Segment\BaseDeg;
 /**
  * Data Element Group: Parameter Depotauftstellung
+ * @link https://www.hbci-zka.de/dokumente/spezifikation_deutsch/fintsv3/FinTS_3.0_Messages_Geschaeftsvorfaelle_2015-08-07_final_version.pdf
+ * Section: O
+ */
  */
 class ParameterDepotaufstellungV2 extends BaseDeg implements ParameterDepotaufstellung
 {
