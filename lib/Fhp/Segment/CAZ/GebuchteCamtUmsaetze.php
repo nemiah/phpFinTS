10c4335171b56fba02b56c8db158b2a1c31cb127
2konrad <59372786+2konrad@users.noreply.github.com>
2022-03-18 16:25:16 +0100

    Inserted link to Issue

--- lib/Fhp/Segment/CAZ/GebuchteCamtUmsaetze.php
+++ lib/Fhp/Segment/CAZ/GebuchteCamtUmsaetze.php
@@ -4,6 +4,13 @@ namespace Fhp\Segment\CAZ;
 
 use Fhp\Segment\BaseDeg;
 
+/**
+ * Needed for
+ * Issue: GetStatementOfAccountXML receiving multiple CAMT XML files
+ * @link: https://github.com/nemiah/phpFinTS/issues/370
+ *
+ * @link: https://github.com/nemiah/phpFinTS/pull/371
+ */
 class GebuchteCamtUmsaetze extends BaseDeg
 {
     /** @var Bin[] @Max(99) */
