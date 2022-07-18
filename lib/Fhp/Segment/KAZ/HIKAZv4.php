41cae7200f54c6397842e927866fb1e43bcc6c8b
Nena Furtmeier <Nena@Furtmeier.IT>
2022-07-18 20:58:55 +0200

    Add option to GetStatementOfAccount::create() to also include unbooked transactions again

--- lib/Fhp/Segment/KAZ/HIKAZv4.php
+++ lib/Fhp/Segment/KAZ/HIKAZv4.php
@@ -26,4 +26,9 @@ class HIKAZv4 extends BaseSegment implements HIKAZ
     {
         return $this->gebuchteUmsaetze;
     }
+
+    public function getNichtGebuchteUmsaetze(): ?Bin
+    {
+        return $this->nichtGebuchteUmsaetze;
+    }
 }
