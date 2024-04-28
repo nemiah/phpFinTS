63373e16f0ae16d5cdcb9c103aff987e76a9abfd
Sebastian Volland <seb@baunz.net>
2024-04-28 19:00:57 +0200

    Fix "Undefined array key" warnings

--- lib/Fhp/Model/StatementOfAccount/StatementOfAccount.php
+++ lib/Fhp/Model/StatementOfAccount/StatementOfAccount.php
@@ -73,11 +73,15 @@ class StatementOfAccount
             } else {
                 $statementModel = new Statement();
                 $statementModel->setDate(static::parseDate($date));
-                $statementModel->setStartBalance((float) $statement['start_balance']['amount']);
-                if(isset($statement['end_balance'])){
+                if (isset($statement['start_balance']['amount'])) {
+                    $statementModel->setStartBalance((float) $statement['start_balance']['amount']);
+                }
+                if (isset($statement['end_balance'])) {
                     $statementModel->setEndBalance((float) $statement['end_balance']['amount']);
                 }
-                $statementModel->setCreditDebit($statement['start_balance']['credit_debit']);
+                if (isset($statement['start_balance']['credit_debit'])) {
+                    $statementModel->setCreditDebit($statement['start_balance']['credit_debit']);
+                }
                 $result->statements[] = $statementModel;
             }
 
