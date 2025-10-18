a88a94a8473025a958144dd6e170554ae55e9c1f
Philipp Keck <git@philippkeck.de>
2025-10-18 22:02:16 +0200

    Add missing initialization of $oneAccount in transfer.php sample

--- Samples/transfer.php
+++ Samples/transfer.php
@@ -21,6 +21,15 @@ use nemiah\phpSepaXml\SEPATransfer;
 /** @var \Fhp\FinTs $fints */
 $fints = require_once 'login.php';
 
+// Just pick the first account, for demonstration purposes. You could also have the user choose, or have SEPAAccount
+// hard-coded and not call getSEPAAccounts() at all.
+$getSepaAccounts = \Fhp\Action\GetSEPAAccounts::create();
+$fints->execute($getSepaAccounts);
+if ($getSepaAccounts->needsTan()) {
+    handleStrongAuthentication($getSepaAccounts); // See login.php for the implementation.
+}
+$oneAccount = $getSepaAccounts->getAccounts()[0];
+
 $dt = new \DateTime();
 $dt->add(new \DateInterval('P1D'));
 
